284162e555c1a6f4174fd4ff8e5939aa2fb03d92
Manh Tien <tienbm12@gitlab.com>
2023-03-30 16:15:39 +0700

    adds laravel

---
+++ README.md
@@ -0,0 +1,31 @@
+
+
+docker-compose up --build 
+
+docker container ps 
+
+docker exec -it {containerID} sh
+
+composer create-project laravel/laravel example-app
+
+cp -rf example-app/ ../ 
+
+rm -rf example-app
+chmod -R 777 storage
+chmod -R 775 bootstrap/cache
+
+
+composer config --global process-timeout 2000
+composer install 
+composer update
+
+php artisan key:generate
+php artisan cache:clear
+php artisan config:cache
+
+docker exec -it php_laravel /bin/bash
+
+
+docker exec -it mysql_laravel /bin/bash
+mysql -u rootUser -p rootDB 
+root_password 
---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('breadcrumb')
+    <div class="page-breadcrumb">
+        <div class="row">
+            <div class="col-5 align-self-center">
+                <h4 class="page-title">Dashboard</h4>
+                <div class="d-flex align-items-center">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Library</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@section('content')
+    <div class="container-fluid">
+        
+    </div>
+@endsection
+
+
